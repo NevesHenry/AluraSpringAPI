2f0ecdae8fd23c174a795ac4ffe965236205671d
NevesHenry <henriquereisneves@hotmail.com>
2023-05-19 10:23:36 -0300

    Patroniza retornos da API com o ResponseEntity

--- src/main/java/med/volkl/api/medico/MedicoRepository.java
+++ src/main/java/med/volkl/api/medico/MedicoRepository.java
@@ -1,5 +1,6 @@
 package med.volkl.api.medico;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
